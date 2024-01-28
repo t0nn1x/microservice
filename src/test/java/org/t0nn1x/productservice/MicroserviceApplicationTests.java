dfc4dd569801d6288e2e85b8f46aba39ec63b057
t0nn1x <antoonhrobust@gmail.com>
2024-01-28 23:52:55 +0100

    Initial commit

---
+++ src/test/java/org/t0nn1x/productservice/MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.t0nn1x.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
